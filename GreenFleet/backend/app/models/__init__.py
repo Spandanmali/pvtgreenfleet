d6670efae5f9fdc6db83776b8a0d74bdb7d130de
Spandanmali <spandanmali2006@gmail.com>
2026-09-24 19:30:46 +0530

    backend deletion

--- GreenFleet/backend/app/models/__init__.py
+++ GreenFleet/backend/app/models/__init__.py
@@ -1,5 +1,1 @@
 from app.models.user import User, UserRole
-from app.models.vessel import Vessel, VesselType, CIIGrade
-from app.models.voyage import Voyage, VoyageStatus
-from app.models.fuel_prediction import FuelPrediction
-from app.models.cii_record import CIIRecord
